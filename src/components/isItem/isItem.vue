ec0dbfa92afb1297130858d536517b3abfd3158f
Park Jinyong <jiny3360@nine2021.com>
2022-08-25 12:01:42 +0900

    feat: settings - Today && item 컴포넌트 생성 - Board 컴포넌트 생성

---
+++ src/components/isItem/isItem.vue
@@ -0,0 +1,11 @@
+<template>
+    item
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/today/todayList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col xl:grid xl:grid-cols-2">
+    <today-item></today-item>
+    <today-item></today-item>
+    <today-item></today-item>
+    <today-item></today-item>
+  </div>
+</template>
+
+<script setup>
+import TodayItem from './todayItem.vue'
+
+</script>
+
+<style scoped></style>
